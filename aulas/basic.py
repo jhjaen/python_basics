ce752a13f8127f634492860549eea2524cf490a3
Jose hugo <jhjaen@hotmail.com>
2020-03-04 22:26:06 -0300

    Aula01

---
+++ aulas/basic.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+nome = 'jose'
+idade = 30
+altura = 1.70
+brasileiro = True
+
+# print('Ola meu nome e', nome)    Primeira opcao de impressao na tela
+# print('Ola meu nome e {}'.format(nome))    #Segunda opcao de impressao na tela
+# print(f'Ola meu nome e {nome}')    #Terceira opcao de impressao na tela
+
+#vendo tipos de dados
+# print(type(nome))
+# print(type(idade))
+# print(type(altura))
+# print(type(brasileiro))
+
+#entrada basica e dados
+# nome = input('Digite seu nome: ')
+# print('O nome do usuario e', nome)
+
+# num1 = float(input('Digite o primeiro numero: '))
+# num2 = int(input('Digite o segundo numero: '))
+# soma = num1+num2
+
+# print(str(soma))
+
+#num1 = float(input('Digite o primeiro numero: '))
+# num2 = int(input('Digite o segundo numero: '))
+# soma = num1+num2
+
+# print(str(soma))
+
+texto ='Este e um texto\
+ completo e grande'
+
+print(texto.upper())
+print(texto.lower())
+print(texto.capitalize())
+print(texto.split(' '))
---
+++ exercicios/basic_ex.py
@@ -0,0 +1,13 @@
+#dontpad.com/5520python
+#1. Crie um script com duas variaveis
+#nome e sobrenome
+#e imprima na tela 
+# a mensagem Ola meu nome e nome sobrenome
+# -*- coding: utf-8 -*-
+
+nome = 'Jose Hugo'
+sobrenome = 'Jaen'
+
+print('Ola meu nome e', nome, sobrenome)
+print('Ola meu nome e {} {}'.format(nome, sobrenome))
+print(f'Ola meu nome e {nome} {sobrenome}')
