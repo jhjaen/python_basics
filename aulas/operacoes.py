42fdb2867c848e2dba16543d4a8e7a51d11d60e3
José Hugo Jaen <jhjaen@hotmail.com>
2020-03-12 22:14:19 -0300

    aula07

---
+++ aulas/operacoes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#Criando um modulo para ser importado
+# def soma(x , y):
+#     print(x + y)
+
+# def subtracao(z , p):
+#     print(z - p)
+
+#Faça uma função que peça um nome e um sobrenome separadamente
+# e imprima Nome: <nome> Sobrenome: <Sobrenome>
+# usar essa funçao em principal
+
+def impNome(nome, sobrenome):
+    #print(f'Nome: {nome} Sobrenome: {sobrenome}')
+    return f'Nome: {nome} Sobrenome: {sobrenome}'
---
+++ aulas/principal.py
@@ -0,0 +1,27 @@
+#! python3
+# -*- coding: utf-8 -*-
+
+#Principal
+
+# import operacoes # importando o modulo inteiro
+# from operacoes import impNome # importando apenas o que eu quero do modulo
+
+# print('Codigo principal!')
+
+# print (impNome('Jose', 'Lima'))
+
+###
+
+# import os
+# import sys
+
+#print(os.listdir(sys.argv[1]))
+
+# print(os.system(f'ping {sys.argv[1]}'))
+# os.system('pause')
+# os.system(f'ping {sys.argv[1]}')
+# os.system('pause')
+
+###
+
+import datetime
